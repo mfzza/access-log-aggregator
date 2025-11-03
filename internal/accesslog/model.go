aa80e097d1fd84136a56c307df51f54ff043d614
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-03 14:49:00 +0700

    rename some variables so it more readable

--- internal/accesslog/model.go
+++ internal/accesslog/model.go
@@ -35,23 +35,3 @@ func NewRecord(jb []byte) (*Record, error) {
 	return &al, nil
 }
 
-func (r *Record) Print() {
-	fmt.Println("===================================================")
-	fmt.Println("time:", r.Time)
-	fmt.Println("host:", r.Host)
-	fmt.Println("status code:", r.StatusCode)
-	fmt.Println("duration:", r.Duration)
-}
-
-func (s *Summary) Print() {
-	fmt.Println(s.host, s.requestTotal, s.request2xx, s.requestTotal - s.request2xx, s.avgDuration)
-}
-
-func (ss *Summaries) Print() {
-	fmt.Println("Host		total_requests  2xx_requests  non_2xx_requests  avg_duration_s")
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-	for _, s := range *ss {
-		s.Print()
-	}
-}
-
