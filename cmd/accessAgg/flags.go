a4ec130614eacebaa46a0ece085cc8cf74f30d8b
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-04 16:53:52 +0700

    feat(flags): detect duplicate in flag `-file`

--- cmd/accessAgg/flags.go
+++ cmd/accessAgg/flags.go
@@ -15,13 +15,20 @@ type Conf struct {
 
 func parseFlags() Conf {
 	var cfg Conf
+	seen := map[string]bool{}
 	flag.Func("file", "path to log file (required)\ncan be specified multiple time, example: -file a.log -file b.log",
 		func(file string) error {
+			if seen[file] {
+				return fmt.Errorf("duplicate file: %s", file) // or emit error?
+				// return nil // skip
+			}
+			seen[file] = true
 			cfg.Files = append(cfg.Files, file)
 			return nil
+
 		})
 
-	flag.BoolVar(&cfg.fromStart,"from-start", false, "optional: read from beginning (default: tail from end)")
+	flag.BoolVar(&cfg.fromStart, "from-start", false, "optional: read from beginning (default: tail from end)")
 	flag.DurationVar(&cfg.Interval, "interval", 10*time.Second, "optional: summary interval")
 
 	flag.Parse()
