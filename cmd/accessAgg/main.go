4c243efe6c9b6c92b15e7c3e9989e3cb3e75aef2
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-04 12:51:00 +0700

    feat(): implement multiple flag `-file`

--- cmd/accessAgg/main.go
+++ cmd/accessAgg/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"accessAggregator/internal/accesslog"
 	"accessAggregator/internal/logreader"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -14,38 +15,52 @@ import (
 
 func main() {
 	// TODO: implement multiple file read with goroutine
-	// TODO: implement flag parameter
 	// TODO: behaviour to default start read from tail (end of file), and read from beginning when have `-from-start` flag
 	// TODO: tolerate common log rotation
 	// TODO: -interval flag
 	// TODO: tail -F like behaviour
 	// TODO: handle graceful exit
 
-	r, err := logreader.NewReader(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to read file: %v\n", err)
-		os.Exit(1)
+	var files []string
+	flag.Func("file", "path to log file (required)\ncan be specified multiple time, example: -file a.log -file b.log",
+		func(file string) error {
+			files = append(files, file)
+			return nil
+		})
+	flag.Parse()
+	if len(files) == 0 {
+		fmt.Fprintln(os.Stderr, "missing required flag: at least one -file <path> must be provided")
+		flag.Usage()
+		os.Exit(2)
 	}
-	defer r.Close()
 
 	ss := accesslog.Summaries{}
-	for i := 1; ; i++ {
 
-		line, err := r.ReadLine()
-		if err == io.EOF {
-			break
-		}
+	for _, file := range files {
+		r, err := logreader.NewReader(file)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
+			fmt.Fprintf(os.Stderr, "Failed to read file: %v\n", err)
+			os.Exit(1)
 		}
+		for {
+			line, err := r.ReadLine()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
 
-		record, err := accesslog.NewRecord(line)
-		if err != nil {
-			// ignore error/malformed/missing field
-			// fmt.Fprintf(os.Stderr, "skipped line: %v\n", err)
-			continue
+			record, err := accesslog.NewRecord(line)
+			if err != nil {
+				// ignore error/malformed/missing field
+				// fmt.Fprintf(os.Stderr, "skipped line: %v\n", err)
+				continue
+			}
+			ss.AddRecord(record)
 		}
-		ss.AddRecord(record)
+		r.Close()
 	}
 	ss.Print()
 }
