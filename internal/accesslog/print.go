012996a92a22356b5724c4ae81edf779b4956113
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-04 08:50:05 +0700

    feat(): improve Print method formatting for Summaries output

--- internal/accesslog/print.go
+++ internal/accesslog/print.go
@@ -1,6 +1,9 @@
 package accesslog
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func (r *Record) Print() {
 	fmt.Println("===================================================")
@@ -10,15 +13,24 @@ func (r *Record) Print() {
 	fmt.Println("duration:", r.Duration)
 }
 
-func (ss *Summaries) Print() {
-	fmt.Println("Host		total_requests  2xx_requests  non_2xx_requests  avg_duration_s")
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-	for k, s := range *ss {
-	// TODO: dont just use \t
-		fmt.Println(k,
-			"\t", s.requestTotal,
-			"\t\t", s.request2xx,
-			"\t\t", s.requestTotal-s.request2xx,
-			"\t\t", s.avgDuration)
+func (ss Summaries) Print() {
+	maxHostLen := 0
+	for h := range ss {
+		l := len(h)
+		if l > maxHostLen {
+			maxHostLen = l
+		}
+	}
+	maxHostLen += 2
+
+	fmt.Printf("%-*s %15s %15s %18s %18s\n",
+		maxHostLen, "Host", "total_requests", "2xx_requests", "non_2xx_requests", "avg_duration_s")
+	fmt.Println(strings.Repeat("-", maxHostLen+70))
+
+	//TODO: maybe try to sort it
+	for h, s := range ss {
+		fmt.Printf("%-*s %15d %15d %18d %18.3f\n",
+			maxHostLen, h, s.requestTotal, s.request2xx,
+			s.requestTotal-s.request2xx, s.avgDuration)
 	}
 }
