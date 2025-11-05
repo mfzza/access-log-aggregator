67faa7b302e1bbdc22596e8ab55e2790c05f1e38
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-05 16:43:21 +0700

    feat(): display current time and added separator on each print

--- internal/accesslog/print.go
+++ internal/accesslog/print.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"time"
 )
 
 func (r *Record) Print() {
@@ -28,6 +29,9 @@ func (ss Summaries) Print() {
 	sort.Strings(hosts)
 	maxHostLen += 2
 
+	fmt.Println()
+	fmt.Println("*** Access Log Summary as of",time.Now().Format("2006-01-02 15:04:05"), "***")
+	fmt.Println(strings.Repeat("=", maxHostLen+70))
 	fmt.Printf("%-*s %15s %15s %18s %18s\n",
 		maxHostLen, "Host", "total_requests", "2xx_requests", "non_2xx_requests", "avg_duration_s")
 	fmt.Println(strings.Repeat("-", maxHostLen+70))
@@ -40,4 +44,5 @@ func (ss Summaries) Print() {
 			ss[h].requestTotal-ss[h].request2xx,
 			ss[h].avgDuration)
 	}
+	fmt.Println(strings.Repeat("-", maxHostLen+70))
 }
