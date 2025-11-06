854d670d8a50a7c63f30d532806e2e52618e25bb
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-06 14:57:34 +0700

    refactor(summaries): split `Print()` method into `format()` to make it testable

--- internal/accesslog/print.go
+++ internal/accesslog/print.go
@@ -7,15 +7,9 @@ import (
 	"time"
 )
 
-func (r *Record) Print() {
-	fmt.Println("===================================================")
-	fmt.Println("time:", r.Time)
-	fmt.Println("host:", r.Host)
-	fmt.Println("status code:", r.StatusCode)
-	fmt.Println("duration:", r.Duration)
-}
+func (ss Summaries) format() string {
+	var b strings.Builder
 
-func (ss Summaries) Print() {
 	// count longest host name, and sorting host name
 	hosts := make([]string, 0, len(ss))
 	maxHostLen := 0
@@ -29,20 +23,26 @@ func (ss Summaries) Print() {
 	sort.Strings(hosts)
 	maxHostLen += 2
 
-	fmt.Println()
-	fmt.Println("*** Access Log Summary as of",time.Now().Format("2006-01-02 15:04:05"), "***")
-	fmt.Println(strings.Repeat("=", maxHostLen+70))
-	fmt.Printf("%-*s %15s %15s %18s %18s\n",
+	fmt.Fprintln(&b)
+	fmt.Fprintln(&b, "*** Access Log Summary as of", time.Now().Format("2006-01-02 15:04:05"), "***")
+	fmt.Fprintln(&b, strings.Repeat("=", maxHostLen+72))
+	fmt.Fprintf(&b, "%-*s %15s %15s %18s %18s\n",
 		maxHostLen, "Host", "total_requests", "2xx_requests", "non_2xx_requests", "avg_duration_s")
-	fmt.Println(strings.Repeat("-", maxHostLen+70))
+	fmt.Fprintln(&b, strings.Repeat("-", maxHostLen+72))
 
 	for _, h := range hosts {
-		fmt.Printf("%-*s %15d %15d %18d %18.3f\n",
+		fmt.Fprintf(&b, "%-*s %15d %15d %18d %18.3f\n",
 			maxHostLen, h,
 			ss[h].requestTotal,
 			ss[h].request2xx,
 			ss[h].requestTotal-ss[h].request2xx,
 			ss[h].avgDuration)
 	}
-	fmt.Println(strings.Repeat("-", maxHostLen+70))
+	fmt.Fprintln(&b, strings.Repeat("=", maxHostLen+72))
+
+	return b.String()
+}
+
+func (ss Summaries) Print() {
+	fmt.Print(ss.format())
 }
