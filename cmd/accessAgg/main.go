d7c5990f0051eeb6f5a223203af2ef0dee427d74
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-13 11:11:27 +0700

    refactor: follow idiomatic pattern for producer-consumer

--- cmd/accessAgg/main.go
+++ cmd/accessAgg/main.go
@@ -2,67 +2,124 @@ package main
 
 import (
 	"accessAggregator/internal/accesslog"
+	"accessAggregator/internal/config"
+	"accessAggregator/internal/tailer"
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/signal"
 	"sync"
-	"syscall"
+	"time"
 )
 
 func main() {
-	flags, err := parseFlags()
+	flags, err := config.ParseFlags()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
+		fmt.Fprintln(os.Stderr, "Error: ", err)
 		flag.Usage()
 		os.Exit(2)
 	}
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-
-	errCh := make(chan error, len(flags.Files))
-	rawRecords := make(chan []byte)
 	ctx, cancel := context.WithCancel(context.Background())
 
-	ss := accesslog.NewSummaries()
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt)
+		sig := <-sigCh
+		fmt.Println("\n\n\nreceive signal:", sig)
+		cancel()
+	}()
 
-	var tailWG sync.WaitGroup
+	summaries := accesslog.NewSummaries()
+	data := make(chan []byte, 100)
 
+	// producer
+	var wg sync.WaitGroup
 	for _, file := range flags.Files {
-		tailWG.Go(func() {
-			if err := streamLogFile(file, flags.fromStart, ctx, rawRecords); err != nil {
-				errorStream := fmt.Errorf("[%s] error tailing: %w", file, err)
-				errCh <- errorStream
+		// NOTE: https://appliedgo.net/spotlight/go-1.25-waitgroup-go/
+		wg.Go(func() {
+			if err := tail(ctx, file, flags.FromStart, data); err != nil {
+				fmt.Fprintf(os.Stderr, "[%s] error tailing: %v", file, err)
 			}
 		})
 	}
 
-	exitCh := make(chan struct{})
-	var aggWG sync.WaitGroup
+	// consumer
+	final := make(chan struct{})
+	go aggr(final, flags, data, summaries)
 
-	aggWG.Go(func() {
-		if ok := aggregateAndPrint(ss, flags, rawRecords, errCh, os.Stdout, os.Stderr); !ok {
-			exitCh <- struct{}{}
+	// wait all tail finish, then close data channel, then wait for aggr end
+	wg.Wait()
+	close(data)
+	<-final
+	fmt.Println("Gracefully shutting down...")
+}
+
+func tail(ctx context.Context, fpath string, fromStart bool, data chan<- []byte) error {
+	tf, err := tailer.NewTailFile(fpath, tailer.OsFS{}, fromStart)
+	if err != nil {
+		return err
+	}
+	defer tf.Close()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			rawRecord, err := tf.GetRawRecord()
+			if err == io.EOF {
+				select {
+				case <-ctx.Done():
+					return nil
+				case <-time.After(100 * time.Millisecond):
+					continue
+				}
+			}
+			if err != nil {
+				return fmt.Errorf("reading record: %w", err)
+			}
+			data <- rawRecord
 		}
-	})
+	}
 
-	select {
-	case <-exitCh:
-		fmt.Fprintln(os.Stderr, "failed to process all file, shutting down...")
-		os.Exit(1)
+}
 
-	case sig := <-sigCh:
-		fmt.Printf("\n\n\nReceived signal: %s.\n", sig)
+func aggr(aggrEnd chan<- struct{}, flags config.Flags, data <-chan []byte, summaries accesslog.Summarizer) {
+	ticker := time.NewTicker(flags.Interval)
+	defer ticker.Stop()
 
-		cancel()
-		tailWG.Wait()
+	var malformRecord int
+	printSummaries := func() {
+		fmt.Fprint(os.Stdout, summaries.Format())
+		if malformRecord > 0 {
+			fmt.Fprintln(os.Stdout, "missing field or malformed log: ", malformRecord)
+		}
+	}
 
-		fmt.Println("Gracefully shutting down... Printing final summary")
+	for {
+		select {
+		case <-ticker.C:
+			printSummaries()
 
-		close(rawRecords)
-
-		aggWG.Wait()
+		// keep process data even after context canceled
+		// to drain remaining data, then give signal
+		// when channel already empty
+		case r, ok := <-data:
+			if !ok {
+				fmt.Println("Printing final summary")
+				printSummaries()
+				close(aggrEnd)
+				return
+			}
+			record, err := accesslog.NewRecord(r)
+			if err != nil {
+				malformRecord++
+				continue
+			}
+			summaries.AddRecord(record)
+		}
 	}
 }
