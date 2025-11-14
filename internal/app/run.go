85b94a38284349258b197ceb19d1d12491e95946
fatkhulizz <fatkhul7izza@gmail.com>
2025-11-14 14:49:22 +0700

    feat(app): make buffer size for data channel scaled instead constant

--- internal/app/run.go
+++ internal/app/run.go
@@ -10,28 +10,31 @@ import (
 )
 
 func Run(ctx context.Context, flags config.Flags, out io.Writer, outErr io.Writer) error {
-    summaries := accesslog.NewSummaries()
-    data := make(chan []byte, 100)
+	summaries := accesslog.NewSummaries()
+
+	// scale with * 25, but min 100 and max 10000
+	bufSize := min(max(len(flags.Files)*25, 100), 10000)
+	data := make(chan []byte, bufSize)
 
 	// producer
-    var wg sync.WaitGroup
-    for _, file := range flags.Files {
-        wg.Go(func() {
-            if err := tail(ctx, file, flags.FromStart, data); err != nil {
-                fmt.Fprintf(outErr, red + "[%s] error: %v\n"+ reset, file, err)
-            }
-        })
-    }
+	var wg sync.WaitGroup
+	for _, file := range flags.Files {
+		wg.Go(func() {
+			if err := tail(ctx, file, flags.FromStart, data); err != nil {
+				fmt.Fprintf(outErr, red+"[%s] error: %v\n"+reset, file, err)
+			}
+		})
+	}
 
 	// consumer
-    aggrDone := make(chan struct{})
-    go aggr(aggrDone, flags, data, summaries, out)
+	aggrDone := make(chan struct{})
+	go aggr(aggrDone, flags, data, summaries, out)
 
-    wg.Wait()
+	wg.Wait()
 
-    close(data)
-    <-aggrDone
+	close(data)
+	<-aggrDone
 
-    fmt.Fprintln(out, "Gracefully shut down...")
-    return nil
+	fmt.Fprintln(out, "Gracefully shut down...")
+	return nil
 }
